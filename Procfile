8f79309428eb1e48b26e657478e7008196679770
Taylor Ninesling <t9sling@hotmail.com>
2017-02-25 00:38:07 -0500

    Updated Procfiles to include port number

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sbt run
+web: sbt run -Dhttp.port=9000
--- test/services/CompilerServiceSpec.scala
+++ test/services/CompilerServiceSpec.scala
@@ -9,11 +9,11 @@ import scala.io.Source
 
 class CompilerServiceSpec extends FlatSpec with Matchers {
   "The compile method" should "run without errors because I said so" in {
-    val uri = s"C:/Users/Taylor/Programs/WhiteboardCompiler/tmp/files/javaFile.java"
+    val uri = s"tmp/files/javaFile.java"
     CompilerService.compileFromURI(uri)
   }
   /*it should "print a line starting with \"Error\"" in {
-    val uri = s"C:/Users/Taylor/Programs/WhiteboardCompiler/tmp/files/badJavaFile.java"
+    val uri = s"tmp/files/badJavaFile.java"
     CompilerService.compileFromURI(uri)
     Source.fromFile(System.out).getLines.foreach { println }
   }*/
